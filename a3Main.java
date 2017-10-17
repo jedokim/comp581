5fb69a3076f7837f117047ad5bb5c13623997878
Jeremy Kim <jedo@live.unc.edu>
2017-10-17 16:27:26 -0400

    Rename Main.java to a3Main.java

---
+++ a3Main.java
@@ -0,0 +1,23 @@
+package comp581a3;
+
+// Team Members:
+// Kyle Dixon (PID: 720464669)
+// Jeremy Kim (PID: 720471249)
+
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
+import java.lang.*;
+import lejos.hardware.*;
+import lejos.hardware.Sound;
+import lejos.hardware.motor.EV3MediumRegulatedMotor;
+import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3TouchSensor;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.hardware.sensor.SensorMode;
+
+public class Main {
+   public static void main(String[] args) {
+   }
+}
